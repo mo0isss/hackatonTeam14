c62166bb1f41b3d8e0511b98d42ddaa40999fa5e
DianaHan <k.dianchic@gmail.com>
2026-06-01 15:12:02 +0300

    Add basic classification tests

---
+++ tests/identifierTest/test_classification.py
@@ -0,0 +1,56 @@
+import pytest
+from src.identifier.classification import Classification
+from src.parser.Email import Email
+
+@pytest.fixture
+def classifier():
+    return Classification()
+
+@pytest.mark.parametrize("body, subject, expected_category",
+        [("Поздравляем! Вы стали победителем в ррозыгрыше. Перейдите по ссылке чтобы получить приз",
+        "Заберите свой приз",
+        "spam"),
+
+        ("Работа vsego otdela приостановлена. Интернет не работает во всём здании",
+        "Очень срочная ситуация",
+        "urgent_cases"),
+
+        ("Мама, не работает код! Дай мне доступ к файлу, я поменяю его. Спасибо",
+        "Прошу выдать разрешение",
+        "access_requests"),
+
+        ("Высылаю реквизиты счёта. Пожалуйста, проверьте их",
+        "Реквизиты счёта в банке",
+        "documents"),
+
+        ("Не включается ноутбук, не знаю что делать, уже всё перепробовала",
+        "Поломка ноутбука",
+        "equipment"),
+
+        ("Немедленно требую продлить мне отпуск, я устала!",
+        "Отпуск Хан Диане",
+        "hr"),
+
+        ("Давайте созвонимся сегодня вечером, а то нам сдавать хакатон уже пора",
+        "Встреча команды 14",
+        "invitation"),
+        
+        ("Высылаю вам итоги за эти 3 месяца",
+        "Итоги квартала",
+        "news_letter"),
+        
+        ("Excel перестал работать, как можно исправить эту проблему?",
+        "Проблема с работой Excel",
+        "software_problem"),
+        
+        ("Проверка сервиса: healthcheck завершился с ошибкой.",
+        "Cистемное уведомление",
+        "system_monitoring"),
+        
+        ("Ксюша, привет! Диана зовет тебя сегодня в гости",
+        "В гости",
+        "other")])
+        
+def test_classification(classifier, body, subject, expected_category):
+    email = Email("hacatonTeam14", body, subject)
+    assert classifier.classify(email) == expected_category
