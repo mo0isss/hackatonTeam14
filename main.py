3e2dd6ad40e13115e25c527e643cf7d8ef59a758
Ксения <klubnikapumba@MacBook-Air-Ksenia-4.local>
2026-06-01 11:24:47 +0300

    main file for our programm and merging all parts of project

---
+++ main.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+from src.parser.EmailParser import EmailParser
+from src.identifier.classification import Classification
+from src.mover.mover import mover
+def main():
+    inbox_path = Path("inbox")
+    if not inbox_path.exists():
+        print("Eror: there is no such folder in directory")
+        return
+    parser = EmailParser()
+    classifier = Classification()
+    letters = list(inbox_path.glob("*.txt"))
+    if len(letters) == 0:
+        print('Inbox folder is empty')
+        return
+    for letter in letters:
+        try:
+            email = parser.parse(str(letter))
+            category = classifier.classify(email)
+            new_path = mover(str(letter), category)
+        except Exception as error:
+            print(f"Error occured: {letter.name}:{error}")
+if __name__ == "__main__":
+    main()
